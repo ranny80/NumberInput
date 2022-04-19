d9abe9e984bc612398e9d352a26a09f6667611df
ranny80 <rannyvuong80@gmail.com>
2022-04-19 13:10:36 -0400

    Update main.cpp

--- NumberInput/main.cpp
+++ NumberInput/main.cpp
@@ -7,51 +7,55 @@ int number;
 
 int main()
 {
-	cout << "Are you ready to input the number? (y/n) ";
-	cin >> response;
+    cout << "Are you ready to input the number? (y/n) ";
+    cin >> response;
 
-	if ((response == 'y') || (response == 'Y'))
-	{
-		cout << "Enter a number between 1 through 15: ";
-		cin >> number;
+    if ((response == 'y') || (response == 'Y'))
+    {
+        cout << "Enter a number between 1 through 15: ";
+        cin >> number;
 
-		if ((number >= 1) && (number <= 15))
-		{
-			cout << "You have successfully inputted the number.";
-		}
-		else
-		{
-			cout << "Invalid number. Please try again.";
+        if ((number >= 1) && (number <= 15))
+        {
+            // It will prompt that the number has been inputted successfully
+            cout << "You have successfully inputted the number.";
+        }
+        else
+        {
+            cout << "Invalid number. Please try again.";
 
-			cout << "Enter a number between 1 through 15: ";
-			cin >> number;
+            cout << "Enter a number between 1 through 15: ";
+            cin >> number;
 
-			if ((number >= 1) && (number <= 15))
-			{
-				cout << "You have successfully inputted the number.";
-			}
-			else
-			{
-				cout << "Invalid number. Please try again.";
+            if ((number >= 1) && (number <= 15))
+            {
+                // It will prompt that the number has been inputted successfully
+                cout << "You have successfully inputted the number.";
+            }
+            else
+            {
+                cout << "Invalid number. Please try again.";
 
-				cout << "Enter a number between 1 through 15: ";
-				cin >> number;
+                cout << "Enter a number between 1 through 15: ";
+                cin >> number;
 
-				if ((number >= 1) && (number <= 15))
-				{
-					cout << "You have successfully inputted the number.";
-				}
-				else
-				{
-					cout << "Too many attempts.";
-				}
-			}
-		}
-	}
-	else
-	{
-		cout << "Successfully cancelled.";
-	}
+                if ((number >= 1) && (number <= 15))
+                {
+                    // It will prompt that the number has been inputted succesfully
+                    cout << "You have successfully inputted the number.";
+                }
+                else
+                {
+                    // Inputting three times will get this message
+                    cout << "Too many attempts.";
+                }
+            }
+        }
+    }
+    else
+    {
+        cout << "Successfully cancelled.";
+    }
 
-	return 0;
+    return 0;
 }
